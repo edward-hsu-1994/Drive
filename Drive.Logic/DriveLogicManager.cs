8b7a06c4919694392b6104a13c06f8408eb47de5
XuPeiYao <st89382000@gmail.com>
2018-09-08 00:28:50 +0800

    加入LogicBase

---
+++ Drive.Logic/DriveLogicManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Drive.Models.EF;
+using XWidget.EFLogic;
+
+namespace Drive.Logic {
+    public class DriveLogicManager : LogicManagerBase<DriveContext> {
+        public DriveLogicManager(DriveContext dbcontext) : base(dbcontext) { }
+    }
+}
---
+++ Drive.Logic/UserLogic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Drive.Models.EF;
+using XWidget.EFLogic;
+
+namespace Drive.Logic {
+    public class UserLogic : LogicBase<DriveContext, User, string> {
+        public UserLogic(DriveLogicManager logicManager) : base(logicManager) { }
+    }
+}
