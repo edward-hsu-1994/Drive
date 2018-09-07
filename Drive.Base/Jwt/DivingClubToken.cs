0555ff042bc63edebf9b03b6dd934d791065cf6e
XuPeiYao <st89382000@gmail.com>
2018-09-08 01:07:04 +0800

    加入基礎庫

---
+++ Drive.Base/Jwt/DivingClubToken.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XWidget.Web.Jwt;
+
+namespace Drive.Base.Jwt {
+    public class DriveToken : IJwtToken<DefaultJwtHeader, MvcIdentityPayload> {
+        /// <summary>
+        /// 主題
+        /// </summary>
+        public static class Subjects {
+            /// <summary>
+            /// 管理介面登入
+            /// </summary>
+            public const string ConsoleLogin = "ConsoleLogin";
+        }
+
+        /// <summary>
+        /// 角色
+        /// </summary>
+        public static class Roles {
+            /// <summary>
+            /// 系統管理員
+            /// </summary>
+            public const string Administrator = "Administrator";
+        }
+
+        public DefaultJwtHeader Header { get; set; }
+        public MvcIdentityPayload Payload { get; set; }
+    }
+}
---
+++ Drive.Base/Mvc/BaseController.cs
@@ -0,0 +1,47 @@
+﻿using Drive.Logic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using XWidget.Web.Exceptions;
+
+namespace Drive.Base.Mvc {
+    /// <summary>
+    /// 基礎控制器
+    /// </summary>
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class BaseController : Controller {
+        /// <summary>
+        /// 邏輯管理器
+        /// </summary>
+        public DriveLogicManager Manager { get; private set; }
+
+        /// <summary>
+        /// 初始化基礎控制器
+        /// </summary>
+        /// <param name="database">資料庫實例</param>
+        public BaseController(DriveLogicManager manager) {
+            this.Manager = manager;
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context) {
+            if (context.Exception != null) {
+                if (context.Exception is ExceptionBase ex) {
+                    var result = Json(ex);
+                    result.StatusCode = ex.StatusCode;
+                    context.Result = result;
+                    context.ExceptionHandled = true;
+                } else {
+                    var result = Json(new UnknowException(context.Exception.Message + "," + context.Exception.InnerException?.Message));
+                    result.StatusCode = 500;
+                    context.Result = result;
+                    context.ExceptionHandled = true;
+                }
+            }
+            base.OnActionExecuted(context);
+        }
+    }
+}
