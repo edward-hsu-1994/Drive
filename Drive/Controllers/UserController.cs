4a28c5cdf367444bca6b13fc49c88cab9e8d13f1
XuPeiYao <st89382000@gmail.com>
2018-09-09 02:20:18 +0800

    實作登入Token配發

--- Drive/Controllers/UserController.cs
+++ Drive/Controllers/UserController.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
+using Drive.Base.Jwt;
 using Drive.Base.Mvc;
+using Drive.FileSystem;
 using Drive.Logic;
+using Drive.Models.EF;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using XWidget.Web.Exceptions;
+using XWidget.Web.Jwt;
 
 namespace Drive.Controllers {
     /// <summary>
@@ -15,10 +22,45 @@ namespace Drive.Controllers {
         public UserController(DriveLogicManager manager) : base(manager) {
         }
 
+        /// <summary>
+        /// 使用者登入
+        /// </summary>
+        /// <returns>存取權杖</returns>
         [AllowAnonymous]
-        [HttpGet]
-        public string Get(string aa) {
-            return aa;
+        [HttpPost("token")]
+        public async Task<string> Authorize(
+            [FromBody]User user) {
+            var targetUser = await Manager.UserLogic.GetAsync(user.Id);
+
+            if (!targetUser.MatchPassword(user.Password)) {
+                throw new AuthorizationException();
+            }
+
+            return BuildToken(user);
+        }
+
+        private string BuildToken(User user) {
+            var tokenModel = new DriveToken() {
+                Header = new DefaultJwtHeader() {
+                    Algorithm = SecurityAlgorithms.HmacSha256
+                },
+                Payload = new MvcIdentityPayload() {
+                    Actor = user.Id,
+                    Issuer = Startup.Configuration.GetSection("JWT:Issuer").Value,
+                    Audience = Startup.Configuration.GetSection("JWT:Audience").Value,
+                    Name = user.Id,
+                    Role = DriveToken.Roles.Administrator,
+                    Subject = DriveToken.Subjects.ConsoleLogin,
+                    IssuedAt = DateTime.Now,
+                    Expires = DateTime.Now.AddHours(12)
+                }
+            };
+
+            var key = new SymmetricSecurityKey(Startup.Configuration.GetSection("JWT:SecureKey")
+                .Value.ToHash<MD5>()
+            );
+
+            return tokenModel.Sign(key);
         }
     }
 }
