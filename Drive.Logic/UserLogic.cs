cc8932b927abdbca640bcd9b7586f12b091c6036
XuPeiYao <st89382000@gmail.com>
2018-09-11 19:48:03 +0800

    設定使用者建立與更新掛勾，設定雜湊

--- Drive.Logic/UserLogic.cs
+++ Drive.Logic/UserLogic.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Drive.Models.EF;
 using XWidget.EFLogic;
 
@@ -8,6 +9,19 @@ namespace Drive.Logic {
     public class UserLogic : LogicBase<DriveContext, User, string> {
         public UserLogic(LogicManagerBase<DriveContext> logicManager) : base(logicManager) {
             CreateIgnoreIdentity = false;
+        }
+
+        public override Task BeforeCreate(User entity, params object[] parameters) {
+            entity.SetPassword(entity.Password);
+            return base.BeforeCreate(entity, parameters);
+        }
+
+        public override async Task BeforeUpdate(User entity, params object[] parameters) {
+            var obj = await GetAsync(entity.Id);
+            if (entity.Password != obj.Password) {
+                entity.SetPassword(entity.Password);
+            }
+            await base.BeforeUpdate(entity, parameters);
         }
     }
 }
